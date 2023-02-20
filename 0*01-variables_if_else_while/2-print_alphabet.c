218e1f0485f9971a4d51fa8d7b603481d9384702
Onde01 <delphinonde@gmail.com>
2023-02-20 08:38:13 -0800

    2-print_alphabet.c

---
+++ 0*01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#iclude <stdio.h>
+
+/**
+ * main - Print the alphabet.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char alp(26) = "abcdefghijklmnopqrstuvwxyz;
+	int i;
+	for (i = 0; 1 < 26, i++)
+	{
+		puchar(alp[1]);
+	}
+	putchar(" /n");
+	return (0);
+}
+
