3c0aaa03e6c4a3e0ad33f2e04842ef55f777d5e0
Onde01 <delphinonde@gmail.com>
2023-09-13 15:30:59 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+#include "main.h"
+
+/**
+ * natural numbres - Print natural numbers below 10 that are multiple of 3 or 5
+ *
+ * Return: Always 0.
+ */
 
 int main(void)
 
@@ -13,5 +20,4 @@ if (i % 3 == 0 || i % 5 == 0)
 sum = sum + i;
 }
 printf("%d\n", sum);
-
 }
