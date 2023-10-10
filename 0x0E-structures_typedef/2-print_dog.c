9a6fdf0afc3800189d58112d908160568d058322
Onde01 <delphinonde@gmail.com>
2023-10-10 14:02:14 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,11 +15,16 @@ void print_dog(struct dog *d)
 		return;
 
 	if (d->name == NULL)
-		d->name = "(nil)";
+		printf("Name: (nil)\n;
+	else
+	printf("Name: %s\n", d->name);
+
 	if (d->owner == NULL)
 		d->owner = "(nil)";
+	if (d->age <0)
+	printf("Age: (nil)\n");
+	else
+	printf("Age: %f\n", d->age);
 
-	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age,
-	d->owner);
 
 }
