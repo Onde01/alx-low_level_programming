1dc861c7855ab44a719da2762cd953133cca70ce
Onde01 <delphonde@gmail.com>
2023-10-19 13:03:11 -0700

    4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -11,13 +11,12 @@ void free_list(list_t *head)
 
 {
 
-	list_t *current_node;
-
-	while ((current_node = head) != NULL)
+	if (head)
 	{
-		head = head->next;
-		free(current_node->str);
-		free(current_node);
+		free_list(head->next);
+		if (head->str)
+		free(head->str);
+		free(head);
 	}
 
 }
